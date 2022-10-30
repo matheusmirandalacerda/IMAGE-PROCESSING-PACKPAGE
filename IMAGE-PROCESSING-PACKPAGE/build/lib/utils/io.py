2a13717d8aa0495b9ba075b3c3886af1e0cd8a08
Matheus <108967989+matheusmirandalacerda@users.noreply.github.com>
2022-10-30 07:03:04 -0300

    Delete IMAGE-PROCESSING-PACKPAGE directory

--- IMAGE-PROCESSING-PACKPAGE/build/lib/utils/io.py
+++
@@ -1,8 +0,0 @@
-from skimage.io import imread, insave
-
-def read_image(path, is_gray = False):
-    image = imread(path, as_gray = is_gray)
-    return image
-
-def save_image(image, path):
-    insave(path, image)
